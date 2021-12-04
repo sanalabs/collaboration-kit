c7b877aa3ed3eccada5e9b4ad97b823128f8464c
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2021-12-04 13:27:46 +0100

    Throw if YType is not attached ot a YDoc

--- y-json/src/y-utils/transact.ts
+++ y-json/src/y-utils/transact.ts
@@ -1,15 +1,18 @@
 import * as Y from 'yjs'
 
 /**
- * Run a transaction within `yType`'s document's transaction, if it is available. Otherwise just run the transaction.
+ * Run a transaction within `yType`'s document's transaction, if it is available. Otherwise throw since Yjs
+ * does not support operating on YTypes that are not attached to a YDoc. See the upstream issue
+ * https://github.com/yjs/yjs/issues/207
  */
 export function transact(
   yType: Y.Map<unknown> | Y.Array<unknown>,
   transaction: () => void,
   origin: unknown,
 ): void {
-  const { doc } = yType
-  if (doc !== null) {
-    doc.transact(transaction, origin)
-  } else transaction()
+  if (!yType.doc) {
+    throw new Error('Cannot operate on an YType that is not attached to a YDoc.')
+  }
+
+  yType.doc.transact(transaction, origin)
 }
