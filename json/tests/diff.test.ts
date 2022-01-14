2dd86c4238282a8d8e0299b4e65c1db16c87e37e
Jesper <jesper@sanalabs.com>
2022-01-14 12:33:02 +0100

    Add generalized diff property

--- json/tests/diff.test.ts
+++ json/tests/diff.test.ts
@@ -1,6 +1,7 @@
 import * as fc from 'fast-check'
 import _ from 'lodash'
-import { diff } from '../src/diff-json'
+import { JsonObject } from '../src'
+import { ArrayOperation, diff, ObjectOperation } from '../src/diff-json'
 import { arbitraryJSONArray, arbitraryJSONObject } from './utils'
 
 describe('patchYJson tests', () => {
@@ -23,6 +24,29 @@ describe('patchYJson tests', () => {
         } else {
           expect(result.operations.length).toBeGreaterThan(0)
         }
+      }),
+    )
+  })
+
+  const object = ({ key, value }: { key: string; value: unknown }): Record<string, unknown> => {
+    if (value === undefined) return {}
+    return { [key]: value }
+  }
+
+  const childDiffs = (left: JsonObject, right: JsonObject): (ArrayOperation | ObjectOperation)[] =>
+    _.chain([...Object.keys(left), ...Object.keys(right)])
+      .uniq()
+      .map(key => diff(object({ key, value: left[key] }), object({ key, value: right[key] })))
+      .map((it): ArrayOperation[] | ObjectOperation[] => it.operations)
+      .flatten()
+      .value()
+
+  it('the diff of two objects is equivalent to the concatenation of the diffs of their children', () => {
+    fc.assert(
+      fc.property(arbitraryJSONObject(), arbitraryJSONObject(), (left, right) => {
+        const rootDiff = diff(left, right).operations
+        const nestedDiffs = childDiffs(left, right)
+        expect(rootDiff).toEqual(nestedDiffs)
       }),
     )
   })
