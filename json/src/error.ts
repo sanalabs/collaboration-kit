a17c0f6fb28d342f27d73b252156113f3b1c529c
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2022-01-17 17:04:13 +0100

    Move to yarn and workspaces

--- json/src/error.ts
+++
@@ -1,5 +0,0 @@
-export class AssertionError extends Error {}
-
-export function mkErr(val: unknown, type: string): AssertionError {
-  return new AssertionError(`Expected ${JSON.stringify(val)} to be ${type}`)
-}
